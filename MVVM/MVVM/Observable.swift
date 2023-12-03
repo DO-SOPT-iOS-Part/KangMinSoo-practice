245d3df07e384ef155b8fdea2271557a5414bf21
Minsoo Kang <alstn38@naver.com>
2023-12-03 16:51:39 +0900

    [Feat] MVVM practice

---
+++ MVVM/MVVM/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  MVVM
+//
+//  Created by 강민수 on 12/2/23.
+//
+
+import Foundation
+
+final class Observable<T> {
+  typealias Listener = (T) -> Void
+  var listener: Listener?
+  
+    var value: T {
+    didSet {
+      listener?(value)
+    }
+  }
+    
+  init(_ value: T) {
+    self.value = value
+  }
+    
+  func bind(listener: Listener?) {
+    self.listener = listener
+    listener?(value)
+  }
+}
---
+++ MVVM/MVVM/SoptMemberModel.swift
@@ -0,0 +1,14 @@
+//
+//  SoptMemberModel.swift
+//  MVVM
+//
+//  Created by 강민수 on 12/2/23.
+//
+
+import Foundation
+
+struct SoptMember {
+    let name: String
+    var winning: Bool
+    var age: Int
+}
